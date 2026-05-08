ea6251e61496de87770ae845b88f859108edc999
Jung Dayeon <102217402+al1kite@users.noreply.github.com>
2026-05-08 13:38:53 +0900

    chore(swagger): OpenAPI 설정 보강 (#20)
    * [BE] chore: OpenAPI 설정 보강 및 spotless 포맷팅 적용 #19
    * [BE] fix: Swagger에서 prod 서버 URL을 프로파일별로 분리 #19

--- src/main/java/com/ddd/webbb/global/config/SwaggerConfig.java
+++ src/main/java/com/ddd/webbb/global/config/SwaggerConfig.java
@@ -2,21 +2,45 @@ package com.ddd.webbb.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${spring.profiles.active:local}")
+    private String activeProfile;
+
     @Bean
     public OpenAPI openAPI() {
         String securitySchemeName = "bearerAuth";
 
+        List<Server> servers = new ArrayList<>();
+        servers.add(new Server().url("http://localhost:8080").description("Local"));
+        if ("prod".equals(activeProfile)) {
+            servers.add(new Server().url("https://api.webbb.site").description("Production"));
+        }
+
         return new OpenAPI()
-                .info(new Info().title("WEBBB API").description("WEBBB 백엔드 API 문서").version("v1"))
+                .info(
+                        new Info()
+                                .title("WEBBB API")
+                                .description("WEBBB 백엔드 API 문서")
+                                .version("v1")
+                                .contact(
+                                        new Contact()
+                                                .name("DDD-13 WEBBB")
+                                                .url(
+                                                        "https://github.com/DDD-Community/DDD-13-WEBBB_BE")))
+                .servers(servers)
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(
                         new Components()
