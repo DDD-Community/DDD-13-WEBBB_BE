986a3e3cc7c5eed1dc2ce12648b607d044b6267d
Jung <102217402+al1kite@users.noreply.github.com>
2026-05-08 09:31:44 +0900

    [BE] fix: Swagger에서 prod 서버 URL을 프로파일별로 분리 #19

--- src/main/java/com/ddd/webbb/global/config/SwaggerConfig.java
+++ src/main/java/com/ddd/webbb/global/config/SwaggerConfig.java
@@ -7,16 +7,27 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${spring.profiles.active:local}")
+    private String activeProfile;
+
     @Bean
     public OpenAPI openAPI() {
         String securitySchemeName = "bearerAuth";
+
+        List<Server> servers = new ArrayList<>();
+        servers.add(new Server().url("http://localhost:8080").description("Local"));
+        if ("prod".equals(activeProfile)) {
+            servers.add(new Server().url("https://api.webbb.site").description("Production"));
+        }
 
         return new OpenAPI()
                 .info(
@@ -29,12 +40,7 @@ public class SwaggerConfig {
                                                 .name("DDD-13 WEBBB")
                                                 .url(
                                                         "https://github.com/DDD-Community/DDD-13-WEBBB_BE")))
-                .servers(
-                        List.of(
-                                new Server().url("http://localhost:8080").description("Local"),
-                                new Server()
-                                        .url("https://api.webbb.site")
-                                        .description("Production")))
+                .servers(servers)
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(
                         new Components()
