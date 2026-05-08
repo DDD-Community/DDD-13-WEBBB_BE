d8be2944c31a595adc1b1874bb3ef7a471b71546
Jung <102217402+al1kite@users.noreply.github.com>
2026-05-08 14:01:05 +0900

    [BE] build: 운영/개발 환경 프로필 분리 #26

--- src/main/java/com/ddd/webbb/global/config/SwaggerConfig.java
+++ src/main/java/com/ddd/webbb/global/config/SwaggerConfig.java
@@ -12,8 +12,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!prod")
 public class SwaggerConfig {
 
     @Value("${spring.profiles.active:local}")
